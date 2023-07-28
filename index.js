8d437ed8400adca83d1795cbb192eacdd4092a31
UtkuBoraEfil <utqbora@hotmail.com>
2023-07-28 17:31:07 +0300

    last

--- index.js
+++ index.js
@@ -6,40 +6,46 @@ let remain = document.querySelector(".remain");
 let result = document.querySelector(".result");
 const lastSection = document.querySelector(".mid");
 const restart = document.querySelector(".restart");
-let count = 0;
 
+let count = 0;
 restart.style.display = "none";
 let guessList = [];
 let n = Math.floor(Math.random() * 100 + 1);
 
-far.textContent = count;
-remain.textContent = 5 - count;
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let value = input.value;
-  guessList.push(value);
   count++;
-  far.textContent = count;
-  remain.textContent = 5 - count;
+  guessList.push(input.value);
 
-  guesses.textContent = guessList.join(" - ");
-  input.value = "";
+  updateDisplay();
+  console.log(`count: ${count}`);
 
-  if (n == value) {
-    result.innerHTML = "Congratulations! You fucking got it right!";
-    lastSection.style.display = "none";
-    restart.style.display = "block";
-  } else if (n != value) {
+  if (count === 5 && n !== Number(input.value)) {
+    endGame(`GAME OVER! Correct number was ${n}`);
+    return;
+  }
+
+  let value = Number(input.value);
+  if (n === value) {
+    endGame("Congratulations! You fucking got it right!");
+  } else {
     if (n > value) {
-      result.innerHTML = "Number is higher ";
+      result.textContent = "Number is higher";
     } else if (n < value) {
-      result.innerHTML = "Number is lower";
+      result.textContent = "Number is lower";
     }
   }
-
-  if (count === 5) {
-    lastSection.style.display = "none";
-    result.innerHTML = "GAME OVER! correct number was " + n;
-    restart.style.display = "block";
-  }
+  input.value = "";
 });
+
+function updateDisplay() {
+  far.textContent = count;
+  remain.textContent = 5 - count;
+  guesses.textContent = guessList.join(", ");
+}
+
+function endGame(message) {
+  result.textContent = message;
+  lastSection.style.display = "none";
+  restart.style.display = "block";
+}
